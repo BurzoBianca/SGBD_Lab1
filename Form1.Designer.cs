c57de857ebe0c5a9602705b7425add3466a9798d
BurzoBianca <bburzo18@gmail.com>
2023-07-17 17:23:10 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace SGBD_Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridViewShops = new System.Windows.Forms.DataGridView();
+            this.dataGridViewAngestellte = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.txtMatNr = new System.Windows.Forms.TextBox();
+            this.txtVorname = new System.Windows.Forms.TextBox();
+            this.txtLohn = new System.Windows.Forms.TextBox();
+            this.labelMatNr = new System.Windows.Forms.Label();
+            this.labelLohn = new System.Windows.Forms.Label();
+            this.labelVorname = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.txtCNP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewShops)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAngestellte)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.connButton_Click);
+            // 
+            // dataGridViewShops
+            // 
+            this.dataGridViewShops.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewShops.Location = new System.Drawing.Point(28, 71);
+            this.dataGridViewShops.Name = "dataGridViewShops";
+            this.dataGridViewShops.RowHeadersWidth = 62;
+            this.dataGridViewShops.RowTemplate.Height = 28;
+            this.dataGridViewShops.Size = new System.Drawing.Size(538, 300);
+            this.dataGridViewShops.TabIndex = 1;
+            this.dataGridViewShops.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewShops_CellClick);
+            // 
+            // dataGridViewAngestellte
+            // 
+            this.dataGridViewAngestellte.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewAngestellte.Location = new System.Drawing.Point(603, 71);
+            this.dataGridViewAngestellte.Name = "dataGridViewAngestellte";
+            this.dataGridViewAngestellte.RowHeadersWidth = 62;
+            this.dataGridViewAngestellte.RowTemplate.Height = 28;
+            this.dataGridViewAngestellte.Size = new System.Drawing.Size(626, 300);
+            this.dataGridViewAngestellte.TabIndex = 2;
+            this.dataGridViewAngestellte.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewAngestellte_CellClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(283, 558);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Insert Angestellte";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(677, 559);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 40);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Delete Angestellte";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(482, 558);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(160, 41);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Update Angestellte";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // txtMatNr
+            // 
+            this.txtMatNr.Location = new System.Drawing.Point(1017, 395);
+            this.txtMatNr.Multiline = true;
+            this.txtMatNr.Name = "txtMatNr";
+            this.txtMatNr.Size = new System.Drawing.Size(156, 29);
+            this.txtMatNr.TabIndex = 6;
+            // 
+            // txtVorname
+            // 
+            this.txtVorname.Location = new System.Drawing.Point(759, 443);
+            this.txtVorname.Multiline = true;
+            this.txtVorname.Name = "txtVorname";
+            this.txtVorname.Size = new System.Drawing.Size(156, 31);
+            this.txtVorname.TabIndex = 9;
+            // 
+            // txtLohn
+            // 
+            this.txtLohn.Location = new System.Drawing.Point(1017, 443);
+            this.txtLohn.Multiline = true;
+            this.txtLohn.Name = "txtLohn";
+            this.txtLohn.Size = new System.Drawing.Size(156, 31);
+            this.txtLohn.TabIndex = 11;
+            // 
+            // labelMatNr
+            // 
+            this.labelMatNr.AutoSize = true;
+            this.labelMatNr.Location = new System.Drawing.Point(948, 398);
+            this.labelMatNr.Name = "labelMatNr";
+            this.labelMatNr.Size = new System.Drawing.Size(60, 20);
+            this.labelMatNr.TabIndex = 14;
+            this.labelMatNr.Text = "MatNr :";
+            // 
+            // labelLohn
+            // 
+            this.labelLohn.AutoSize = true;
+            this.labelLohn.Location = new System.Drawing.Point(954, 449);
+            this.labelLohn.Name = "labelLohn";
+            this.labelLohn.Size = new System.Drawing.Size(57, 20);
+            this.labelLohn.TabIndex = 15;
+            this.labelLohn.Text = "Lohn : ";
+            // 
+            // labelVorname
+            // 
+            this.labelVorname.AutoSize = true;
+            this.labelVorname.Location = new System.Drawing.Point(667, 446);
+            this.labelVorname.Name = "labelVorname";
+            this.labelVorname.Size = new System.Drawing.Size(86, 20);
+            this.labelVorname.TabIndex = 16;
+            this.labelVorname.Text = "Vorname : ";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(759, 397);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(156, 29);
+            this.txtName.TabIndex = 17;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(690, 397);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(63, 20);
+            this.labelName.TabIndex = 18;
+            this.labelName.Text = "Name : ";
+            // 
+            // txtCNP
+            // 
+            this.txtCNP.Location = new System.Drawing.Point(759, 487);
+            this.txtCNP.Multiline = true;
+            this.txtCNP.Name = "txtCNP";
+            this.txtCNP.Size = new System.Drawing.Size(156, 30);
+            this.txtCNP.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(700, 490);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "CNP : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 626);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCNP);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.labelVorname);
+            this.Controls.Add(this.labelLohn);
+            this.Controls.Add(this.labelMatNr);
+            this.Controls.Add(this.txtLohn);
+            this.Controls.Add(this.txtVorname);
+            this.Controls.Add(this.txtMatNr);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridViewAngestellte);
+            this.Controls.Add(this.dataGridViewShops);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Konditorei";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewShops)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAngestellte)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridViewShops;
+        private System.Windows.Forms.DataGridView dataGridViewAngestellte;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox txtMatNr;
+        private System.Windows.Forms.TextBox txtVorname;
+        private System.Windows.Forms.TextBox txtLohn;
+        private System.Windows.Forms.Label labelMatNr;
+        private System.Windows.Forms.Label labelLohn;
+        private System.Windows.Forms.Label labelVorname;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox txtCNP;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,397 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SGBD_Lab1
+{
+    public partial class Form1 : Form
+    {
+        static string connectionString = "Data Source=LAPTOP-957DU9BE\\SQLEXPRESS;Initial Catalog=Konditorei;" + "Integrated Security=true";
+        SqlConnection connection = new SqlConnection(connectionString);
+        private int param;
+
+        private int get() { return param; }
+        private void set(int p) { param = p; }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void BindGridShops()
+        {
+            string connectionString = "Data Source=LAPTOP-957DU9BE\\SQLEXPRESS;Initial Catalog=Konditorei;" + "Integrated Security=true";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (connection)
+                {
+                    using (SqlCommand cmd = new SqlCommand("SELECT * FROM Shops", connection))
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                        {
+                            using (DataTable dt = new DataTable())
+                            {
+                                sda.Fill(dt);
+                                dataGridViewShops.DataSource = dt;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private void BindGridAngestellte()
+        {
+            string connectionString = "Data Source=LAPTOP-957DU9BE\\SQLEXPRESS;Initial Catalog=Konditorei;" + "Integrated Security=true";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (connection)
+                {
+                    int shopID = (int)dataGridViewShops.Rows[get()].Cells[2].Value;
+                    using (SqlCommand cmd = new SqlCommand("SELECT * FROM Angestellte WHERE ShopID = @shopID", connection))
+                    {
+                        cmd.Parameters.AddWithValue("@shopID", shopID);
+                        using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                        {
+                            using (DataTable dt = new DataTable())
+                            {
+                                sda.Fill(dt);
+                                dataGridViewAngestellte.DataSource = dt;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void connButton_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=LAPTOP-957DU9BE\\SQLEXPRESS;Initial Catalog=Konditorei;" + "Integrated Security=true";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    BindGridShops();
+
+                    MessageBox.Show("yay");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.ReadLine();
+            }
+
+
+        }
+
+
+        private void dataGridViewShops_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            set(e.RowIndex);
+            dataGridViewAngestellte_CellClick(sender, e);
+            //connectShopsAngestellte();
+
+        }
+
+        private void connectShopsAngestellte()
+        {
+            string connectionString = "Data Source=LAPTOP-957DU9BE\\SQLEXPRESS;Initial Catalog=Konditorei;" + "Integrated Security=true";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlConnection conn = new SqlConnection(connectionString);
+                SqlConnection connect = new SqlConnection(connectionString);
+
+
+                DataSet shopAngestellte = new DataSet();
+
+                SqlDataAdapter shopAdapter = new SqlDataAdapter("SELECT * FROM Shops", conn);
+                SqlDataAdapter angestellteAdapter = new SqlDataAdapter("SELECT * FROM Angestellte", connect);
+
+
+                shopAdapter.Fill(shopAngestellte, "Shops");
+                angestellteAdapter.Fill(shopAngestellte, "Angestellte");
+
+                DataRelation relation = shopAngestellte.Relations.Add("Shops_Angestellte",
+                  shopAngestellte.Tables["Shops"].Columns["ID"],
+                  shopAngestellte.Tables["Angestellte"].Columns["ShopID"]);
+
+                BindingSource bsShops = new BindingSource();
+                bsShops.DataSource = shopAngestellte;
+                bsShops.DataMember = "Shops";
+
+                BindingSource bsAngestellte = new BindingSource();
+                bsAngestellte.DataSource = bsShops;
+                bsAngestellte.DataMember = "Shops_Angestellte";
+
+                dataGridViewShops.DataSource = bsShops;
+                dataGridViewAngestellte.DataSource = bsAngestellte;
+            }
+        }
+
+        private void dataGridViewAngestellte_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            BindGridAngestellte();
+            //exception handler for not binding the second pressed cell with the first one
+            if (txtName.DataBindings.Count > 0)
+                txtName.DataBindings.RemoveAt(0);
+
+            //binds cell to text box (zeigt Name)
+            Binding b1 = new Binding("Text", dataGridViewAngestellte[0, e.RowIndex], "Value", false);
+            txtName.DataBindings.Add(b1);
+
+
+            if (txtVorname.DataBindings.Count > 0)
+                txtVorname.DataBindings.RemoveAt(0);
+
+            //binds cell to text box (zeigt Vorname)
+            Binding b2 = new Binding("Text", dataGridViewAngestellte[1, e.RowIndex], "Value", false);
+            txtVorname.DataBindings.Add(b2);
+
+
+            if (txtCNP.DataBindings.Count > 0)
+                txtCNP.DataBindings.RemoveAt(0);
+
+            //binds cell to text box (zeigt CNP)
+            Binding b3 = new Binding("Text", dataGridViewAngestellte[2, e.RowIndex], "Value", false);
+            txtCNP.DataBindings.Add(b3);
+
+            if (txtMatNr.DataBindings.Count > 0)
+                txtMatNr.DataBindings.RemoveAt(0);
+
+            //binds cell to text box (zeigt MatNr)
+            Binding b4 = new Binding("Text", dataGridViewAngestellte[3, e.RowIndex], "Value", false);
+            txtMatNr.DataBindings.Add(b4);
+
+            if (txtLohn.DataBindings.Count > 0)
+                txtLohn.DataBindings.RemoveAt(0);
+
+            //binds cell to text box (zeigt Lohn)
+            Binding b5 = new Binding("Text", dataGridViewAngestellte[4, e.RowIndex], "Value", false);
+            txtLohn.DataBindings.Add(b5);
+
+        }
+
+        private void GetData()
+        {
+            string sql = "select * from Angestellte ";
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dataGridViewAngestellte.DataSource = dt;
+        }
+        void InitData()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (connection)
+                {
+                    int shopID = (int)dataGridViewShops.Rows[get()].Cells[2].Value;
+                    using (SqlCommand cmd = new SqlCommand("select * from Angestellte where ShopID = @shopID", connection))
+                    {
+                        cmd.Parameters.AddWithValue("@ShopID", shopID);
+                        using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                        {
+                            using (DataTable dt = new DataTable())
+                            {
+                                sda.Fill(dt);
+                                dataGridViewAngestellte.DataSource = dt;
+                            }
+                        }
+                    }
+
+                }
+            }
+        } 
+
+        private void frmContact_Load(object sender, EventArgs e)
+        {
+            InitData();
+        }
+        void Clear()
+        {
+            txtCNP.Text = string.Empty;
+            txtName.Text = string.Empty;
+            txtMatNr.Text = string.Empty;
+            txtLohn.Text = string.Empty;
+            txtVorname.Text = string.Empty;
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    if (String.IsNullOrEmpty(txtLohn.Text) && String.IsNullOrEmpty(txtMatNr.Text) && String.IsNullOrEmpty(txtName.Text) && String.IsNullOrEmpty(txtVorname.Text))
+                    {
+                        MessageBox.Show("Please fill all the fields in");
+                    }
+                    else
+                    {
+                        int shopID = (int)dataGridViewShops.Rows[get()].Cells[2].Value;
+                        if (connection.State == ConnectionState.Closed)
+                            connection.Open();
+
+                        bool matNrIsUnique = CheckUniqueValueInsert(txtMatNr.Text);
+                        if (matNrIsUnique == false)
+                        {
+                            MessageBox.Show("MatNr must be unique");
+                        }
+                        else
+                        {
+                            using (SqlCommand cmd = new SqlCommand("insert into angestellte(MatNr, Lohn, ShopID, Name, Vorname, CNP) values(@MatNr, @Lohn, @ShopID, @Name, @Vorname, @CNP)", connection))
+                            {
+                                cmd.Parameters.AddWithValue("ShopID", shopID);
+                                cmd.Parameters.AddWithValue("@MatNr", txtMatNr.Text);
+                                cmd.Parameters.AddWithValue("@Lohn", txtLohn.Text);
+                                cmd.Parameters.AddWithValue("@Vorname", txtVorname.Text);
+                                cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                                cmd.Parameters.AddWithValue("@CNP", txtCNP.Text);
+                                cmd.ExecuteNonQuery();
+                                InitData();
+                                Clear();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataGridViewRow row = dataGridViewAngestellte.Rows[dataGridViewAngestellte.CurrentRow.Index] as DataGridViewRow;
+                if (row != null)
+                {
+                    if (MessageBox.Show("Are you sure want to delete this record?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        using (SqlConnection connection = new SqlConnection(connectionString))
+                        {
+                            int param = Int32.Parse(txtMatNr.Text);
+                            if (connection.State == ConnectionState.Closed)
+                                connection.Open();
+                            using (SqlCommand cmd = new SqlCommand("delete from Angestellte where MatNr = @p", connection))
+                            {
+                                cmd.Parameters.AddWithValue("@p", param);
+                                cmd.ExecuteNonQuery();
+                                InitData();
+                                Clear();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    int shopID = (int)dataGridViewShops.Rows[get()].Cells[2].Value;
+                    int matNr = Int32.Parse(txtMatNr.Text);
+                    bool matNrIsUnique = CheckUniqueValueUpdate(txtMatNr.Text);
+                    if (matNrIsUnique == false)
+                    {
+                        MessageBox.Show("MatNr must be unique");
+                    }
+                    else
+                    {
+                        if (connection.State == ConnectionState.Closed)
+                            connection.Open();
+                        using (SqlCommand cmd = new SqlCommand("update Angestellte set CNP = @CNP, Lohn = @Lohn, Vorname = @Vorname, Name = @Name where ShopID = @shopID and MatNr = @matNr", connection))
+                        {
+                            DataGridViewRow row = dataGridViewAngestellte.Rows[dataGridViewAngestellte.CurrentRow.Index] as DataGridViewRow;
+                            if (row != null)
+                            {
+                                cmd.Parameters.AddWithValue("@shopID", shopID);
+                                cmd.Parameters.AddWithValue("@matNr", matNr);
+                                cmd.Parameters.AddWithValue("@Lohn", txtLohn.Text);
+                                cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                                cmd.Parameters.AddWithValue("@Vorname", txtVorname.Text);
+                                cmd.Parameters.AddWithValue("@CNP", txtCNP.Text);
+                                cmd.ExecuteNonQuery();
+                                InitData();
+                                Clear();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public bool CheckUniqueValueUpdate(string matNr)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                if (connection.State == ConnectionState.Closed)
+                    connection.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT COUNT(*) from Angestellte where MatNr = @MatNr ", connection))
+                {
+                    cmd.Parameters.AddWithValue("@MatNr", matNr);
+                    cmd.ExecuteNonQuery();
+
+                    var result = Convert.ToUInt16(cmd.ExecuteScalar());
+                    connection.Close();
+
+                    if (result == 1)
+                        return true;
+                    else
+                        return false;
+                }
+
+            }
+        }public bool CheckUniqueValueInsert(string matNr)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                if (connection.State == ConnectionState.Closed)
+                    connection.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT COUNT(*) from Angestellte where MatNr = @MatNr ", connection))
+                {
+                    cmd.Parameters.AddWithValue("@MatNr", matNr);
+                    cmd.ExecuteNonQuery();
+
+                    var result = Convert.ToUInt16(cmd.ExecuteScalar());
+                    connection.Close();
+
+                    if (result == 0)
+                        return true;
+                    else
+                        return false;
+                }
+
+            }
+        }
+    }
+}
